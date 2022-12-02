6f442cadbca465b9b04775c84312c6405068f073
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2022-12-02 13:22:26 -0300

    1

--- php/api/index.php
+++ php/api/index.php
@@ -1,4 +1,18 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+$xml  = file_get_contents("https://www.youtube.com/feeds/videos.xml?playlist_id=PL9tjqljCWvB3dF-Zo16jHwaAvCt6uzcGZ");
+
+$xmldata = simplexml_load_string($xml);
+ 
+$jsondata = json_encode($xmldata);
+
+file_put_contents("api.json", $jsondata);
+
+header("location: /api.json");
+
+//echo "<a href='/arquivo.json' target='_blank'>/arquivo.json</a>";
+
+//print_r($jsondata);  https://rss.app/feeds/GY72k3NLXlP0da2m.xml
+
+
+?>
